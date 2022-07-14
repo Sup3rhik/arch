689bdf095c132585277d92d5e88250174ea22ed8
Superhik <94892775+Sup3rhik@users.noreply.github.com>
2022-07-14 11:59:52 +0200

    Update apps-kl.sh

--- apps-kl.sh
+++ apps-kl.sh
@@ -82,7 +82,7 @@ echo
 
 #-------------------------------------------------------------------------
 
-paru -S --noconfirm aic94xx-firmware wd719x-firmware packagekit-qt5 gst-plugin-libde265 peazip-qt5 latte-dock-git ckb-next etcher-bin capt-src mangohud lib32-mangohud heroic-games-launcher-bin proton-ge-custom-bin lutris-git spotify teamviewer zramd vivaldi
+paru -S --noconfirm aic94xx-firmware wd719x-firmware packagekit-qt5 gst-plugin-libde265 peazip-qt5 latte-dock-git ckb-next etcher-bin capt-src mangohud lib32-mangohud heroic-games-launcher-bin proton-ge-custom-bin lutris-git spotify teamviewer zramd vivaldi brave-bin
 
 #-------------------------------------------------------------------------
 
