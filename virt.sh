ca782d85a1eb6a7c8bbdcf1ef54d0ce69cacb867
Superhik <94892775+Sup3rhik@users.noreply.github.com>
2022-07-14 15:52:42 +0200

    Add files via upload

--- virt.sh
+++ virt.sh
@@ -34,8 +34,34 @@ echo "Done!"
 echo
 
 #-------------------------------------------------------------------------
+tar -xzvf hooks.tar.gz
+sudo mkdir -p /etc/libvirt/hooks
+sudo wget 'https://raw.githubusercontent.com/PassthroughPOST/VFIO-Tools/master/libvirt_hooks/qemu' \
+     -O /etc/libvirt/hooks/qemu
+sudo chmod +x /etc/libvirt/hooks/qemu
+sudo mkdir -p /etc/libvirt/hooks/qemu.d/Windows/prepare/begin
+sudo mkdir -p /etc/libvirt/hooks/qemu.d/Windows/release/end
+
+sudo cp start.sh /etc/libvirt/hooks/qemu.d/Windows/prepare/begin/
+sudo chmod +x /etc/libvirt/hooks/qemu.d/Windows/prepare/begin/start.sh
+
+sudo cp revert.sh /etc/libvirt/hooks/qemu.d/Windows/release/end/
+sudo chmod +x /etc/libvirt/hooks/qemu.d/Windows/release/end/revert.sh
+
+#sed -i '/MODULES=()/c \MODULES=(btrfs)' /etc/libvirt/hooks/kvm.conf
+sudo echo "VIRSH_GPU_VIDEO=pci_0000_01_00_0" >> /etc/libvirt/hooks/kvm.conf
+sudo echo "VIRSH_GPU_AUDIO=pci_0000_01_00_1" >> /etc/libvirt/hooks/kvm.conf
+
 sudo sed -i '81s/.//' /etc/libvirt/libvirtd.conf
 sudo sed -i '104s/.//' /etc/libvirt/libvirtd.conf
+sudo echo "log_filters="1:qemu"" >> /etc/libvirt/libvirtd.conf
+sudo echo "log_outputs="1:file:/var/log/libvirt/libvirtd.log"" >> /etc/libvirt/libvirtd.conf
+
+sudo usermod -a -G libvirt $(whoami)
+
+sed -i '/#user = "libvirt-qemu"/c \user = "ivo"' /etc/mkinitcpio.conf
+sed -i '/#group = "libvirt-qemu"/c \group = "wheel"' /etc/mkinitcpio.conf
+sudo mkdir -p /var/lib/libvirt/vbios
 
 #-------------------------------------------------------------------------
 
@@ -59,5 +85,5 @@ echo "  net-start enabled"
 
 echo
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
-sleep 5
-reboot
+#sleep 5
+#reboot
