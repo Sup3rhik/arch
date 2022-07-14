9d4a141154c5707da28ba17327a09e52db09b0a7
Superhik <94892775+Sup3rhik@users.noreply.github.com>
2022-07-14 16:22:31 +0200

    Update virt.sh

--- virt.sh
+++ virt.sh
@@ -48,9 +48,12 @@ sudo chmod +x /etc/libvirt/hooks/qemu.d/Windows/prepare/begin/start.sh
 sudo cp revert.sh /etc/libvirt/hooks/qemu.d/Windows/release/end/
 sudo chmod +x /etc/libvirt/hooks/qemu.d/Windows/release/end/revert.sh
 
-#sed -i '/MODULES=()/c \MODULES=(btrfs)' /etc/libvirt/hooks/kvm.conf
-sudo echo "VIRSH_GPU_VIDEO=pci_0000_01_00_0" >> /etc/libvirt/hooks/kvm.conf
-sudo echo "VIRSH_GPU_AUDIO=pci_0000_01_00_1" >> /etc/libvirt/hooks/kvm.conf
+sed -i '$ a\VIRSH_GPU_CPU=pci_0000_00_01_0' /etc/libvirt/hooks/kvm.conf
+sed -i '$ a\VIRSH_GPU_VIDEO=pci_0000_01_00_0' /etc/libvirt/hooks/kvm.conf
+sed -i '$ a\VIRSH_GPU_AUDIO=pci_0000_01_00_1' /etc/libvirt/hooks/kvm.conf
+sed -i '$ a\VIRSH_GPU_USB=pci_0000_01_00_2' /etc/libvirt/hooks/kvm.conf
+sed -i '$ a\VIRSH_GPU_SBC=pci_0000_01_00_3' /etc/libvirt/hooks/kvm.conf
+
 
 sudo sed -i '81s/.//' /etc/libvirt/libvirtd.conf
 sudo sed -i '104s/.//' /etc/libvirt/libvirtd.conf
