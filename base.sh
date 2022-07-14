4b104a83f1d260e4e897c7f6d92c4505786512cc
Superhik <94892775+Sup3rhik@users.noreply.github.com>
2022-07-14 09:29:17 +0200

    Add files via upload

--- base.sh
+++ base.sh
@@ -26,24 +26,17 @@ echo root:passwd | chpasswd
 
 #----------------------------------SUDO-----------------------------------
 
-# Add sudo no password rights
-#sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
-#sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 #----------------------------------PARU-----------------------------------
 
 #Enable multilib
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-git clone https://aur.archlinux.org/paru-bin
-cd paru-bin
-pacman -Syu
-makepkg -si
 
 #----------------------------------APPS-------------------------------------
 
 pacman -Sy --noconfirm --needed
-pacman -S btrfs-progs base-devel linux-zen-headers linux-firmware grub efibootmgr dosfstools os-prober mtools networkmanager dialog wpa_supplicant wireless_tools nano wget reflector snapper dolphin konsole rsync ark unzip ntfs-3g kate bash-completion sof-firmware flatpak kinit ttf-droid ttf-hack ttf-font-awesome otf-font-awesome ttf-lato ttf-liberation ttf-linux-libertine ttf-opensans ttf-roboto ttf-ubuntu-font-family terminus-font ufw cronie ksysguard htop kfind sshfs samba openssh nfs-utils cups nmap print-manager cups-pdf grub-customizer
-#pacman -S --noconfirm nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings
+pacman -S --noconfirm btrfs-progs base-devel linux-zen-headers linux-firmware grub efibootmgr dosfstools os-prober mtools networkmanager dialog wpa_supplicant wireless_tools nano wget reflector snapper dolphin konsole rsync ark unzip ntfs-3g kate bash-completion sof-firmware flatpak kinit ttf-droid ttf-hack ttf-font-awesome otf-font-awesome ttf-lato ttf-liberation ttf-linux-libertine ttf-opensans ttf-roboto ttf-ubuntu-font-family terminus-font ufw cronie ksysguard htop kfind sshfs samba openssh nfs-utils cups nmap print-manager cups-pdf grub-customizer
 
 #----------------------------------GRUB-------------------------------------
 
@@ -78,24 +71,6 @@ echo "Numlock=On" >> /etc/sddm.conf
 sed -i '12s/.//' /etc/profile.d/freetype2.sh
 echo "PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"" >> /etc/environment
 echo "EDITOR="/usr/bin/vim"" >> /etc/environment
-
-#----------------------------------THEME----------------------------------
-
-tar -xzvf config.tar.gz
-tar -xzvf icons.tar.gz
-tar -xzvf local.tar.gz
-tar -xzvf Omen.tar.gz
-tar -xzvf Dragon.tar.gz
-mv .config ~/
-mv .icons ~/
-mv .local ~/
-
-sudo mv Omen /usr/share/sddm/themes/
-sudo mv Dragon /usr/share/sddm/themes/
-sudo rm -rf /usr/share/sddm/themes/maldives
-sudo rm -rf /usr/share/sddm/themes/maya
-sudo rm -rf /usr/share/sddm/themes/elarun
-sudo rm -rf /usr/share/sddm/themes/breeze
 
 #----------------------------------KDE------------------------------------
 
