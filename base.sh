d095959e62aa2d20562c1e0353c69458bdc4281d
Superhik <94892775+Sup3rhik@users.noreply.github.com>
2022-07-15 23:08:38 +0200

    Update base.sh

--- base.sh
+++ base.sh
@@ -99,9 +99,9 @@ sleep 1
 
 #----------------------------------THEME----------------------------------
 
-tar -xzvf config.tar.gz /home/ivo
-tar -xzvf icons.tar.gz /home/ivo
-tar -xzvf local.tar.gz /home/ivo
+tar -C /home/ivo/ -xzvf config.tar.gz
+tar -C /home/ivo/ -xzvf icons.tar.gz
+tar -C /home/ivo/ -xzvf local.tar.gz
 
 rm -rf /usr/share/sddm/themes/maldives
 rm -rf /usr/share/sddm/themes/maya
