53725d888ab86313e4219149f03b139656722108
Superhik <94892775+Sup3rhik@users.noreply.github.com>
2022-07-18 13:36:55 +0200

    Add files via upload

--- paru.sh
+++ paru.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
-#-------------------------------------------------------------------------
-
-sudo pacman -Syu --noconfirm
 
 #-----------------------------------PARU----------------------------------
 
 git clone https://aur.archlinux.org/paru-bin
 cd paru-bin
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 makepkg -si --noconfirm
-cd ..
 
 # ---------------------------------APPS-----------------------------------
 
 paru -S --noconfirm aic94xx-firmware wd719x-firmware brave-bin zramd
 sudo systemctl enable --now zramd.service
-echo "  SWAP enabled and started "
-
-lookandfeeltool -a org.kde.breezedark.desktop
-/usr/lib/plasma-changeicons BeautyLine
+echo "  SWAP enabled and started"
 
 echo
-/bin/echo -e "\e[1;32mD O N E\e[0m"
+/bin/echo -e "\e[1;32mDONE & REBOOTING IN 5..4..3..2..1..\e[0m"
+sleep 5
+reboot
