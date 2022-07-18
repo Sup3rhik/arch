aa956d018ff094b5b85f46fd0d0976537670cf03
Superhik <94892775+Sup3rhik@users.noreply.github.com>
2022-07-18 17:28:12 +0200

    Add files via upload

--- apps-kl.sh
+++ apps-kl.sh
@@ -3,32 +3,35 @@
 
 sudo pacman -S --noconfirm archlinux-keyring
 sudo pacman -Syu --noconfirm
+tar -xzvf Archive.tar.gz
 
-git clone https://aur.archlinux.org/paru-bin
-cd paru-bin
-sudo pacman -Syu
-makepkg -si
-cd ..
-
-# ---------------------------------APPS-PACMAN----------------------------------
+#---------------------------------APPS-PACMAN-----------------------------
 
 PKGS=(
 
+    # NVIDIA---------------------------------------------------------------
+
+#    'nvidia-dkms'
+#    'nvidia-utils'
+#    'lib32-nvidia-utils'
+#    'nvidia-settings'
+
     # Wine ----------------------------------------------------------------
 
-    'wine-staging'
+    'wine'
+    'winetricks'
     'wine-mono'
     'wine-gecko'
 
     # GRAPHICS AND DESIGN -------------------------------------------------
 
     'gcolor2'
-    'gimp'
+    'gimp'   
     'inkscape'
     'imagemagick'
-    'gwenview'
-    'spectacle'
-    'okular'
+    'gwenview'   
+    'spectacle'  
+    'okular'     
 
     # GAMING --------------------------------------------------------------
 
@@ -42,6 +45,9 @@ PKGS=(
     'neofetch'
     'qbittorrent'
     'kvantum'
+    'kdeconnect'
+    'stress'
+    'packagekit-qt5'
 
     # DISK UTILITIES ------------------------------------------------------
 
@@ -52,7 +58,7 @@ PKGS=(
     # Office --------------------------------------------------------------
 
     'libreoffice'
-    'korganizer'
+    'korganizer' 
     'gnucash'
     'nextcloud-client'
     'kcalc'
@@ -62,6 +68,25 @@ PKGS=(
     'gnome-subtitles'
     'vlc'
     'mkvtoolnix-gui'
+
+    # SCANER --------------------------------------------------------------
+
+#    'sane-airscan'
+#    'ipp-usb'
+#    'skanlite'
+
+    # VIRT MANAGER---------------------------------------------------------
+
+    'qemu'
+    'libvirt'
+    'edk2-ovmf'
+    'virt-manager'
+    'vde2'
+    'dnsmasq'
+    'bridge-utils'
+    'ovmf'
+    'openbsd-netcat'
+
 
 )
 
@@ -76,8 +101,58 @@ echo
 
 # ---------------------------------APPS-PARU--------------------------------------
 
-paru -S --noconfirm aic94xx-firmware wd719x-firmware packagekit-qt5 gst-plugin-libde265 peazip-qt5 latte-dock-git ckb-next vivaldi brave-bin
-paru -S --noconfirm etcher-bin mangohud lib32-mangohud heroic-games-launcher-bin proton-ge-custom-bin lutris-git spotify teamviewer zramd
+paru -S --noconfirm  gst-plugin-libde265 peazip-qt5 latte-dock-git ckb-next vivaldi etcher-bin mangohud lib32-mangohud heroic-games-launcher-bin proton-ge-custom-bin protonup-qt lutris-git spotify teamviewer auto-cpufreq capt-src
+
+#----------------------------------VIRT MANAGER-----------------------------------
+
+sudo mkdir -p /etc/libvirt/hooks
+sudo wget 'https://raw.githubusercontent.com/PassthroughPOST/VFIO-Tools/master/libvirt_hooks/qemu' \
+     -O /etc/libvirt/hooks/qemu
+sudo chmod +x /etc/libvirt/hooks/qemu
+sudo mkdir -p /etc/libvirt/hooks/qemu.d/Windows/prepare/begin
+sudo mkdir -p /etc/libvirt/hooks/qemu.d/Windows/release/end
+
+sudo tar -C /etc/libvirt/hooks/qemu.d/Windows/prepare/begin/ -xzvf start.tar.gz
+rm start.tar.gz
+sudo chmod +x /etc/libvirt/hooks/qemu.d/Windows/prepare/begin/start.sh
+
+sudo tar -C /etc/libvirt/hooks/qemu.d/Windows/release/end/ -xzvf revert.tar.gz
+rm revert.tar.gz
+sudo chmod +x /etc/libvirt/hooks/qemu.d/Windows/release/end/revert.sh
+
+sudo touch /etc/libvirt/hooks/kvm.conf
+sudo gawk -i inplace 'BEGINFILE{print" "} 1' /etc/libvirt/hooks/kvm.conf
+sudo sed -i '$ a\VIRSH_GPU_CPU=pci_0000_00_01_0' /etc/libvirt/hooks/kvm.conf
+sudo sed -i '$ a\VIRSH_GPU_VIDEO=pci_0000_01_00_0' /etc/libvirt/hooks/kvm.conf
+sudo sed -i '$ a\VIRSH_GPU_AUDIO=pci_0000_01_00_1' /etc/libvirt/hooks/kvm.conf
+#sudo sed -i '$ a\VIRSH_GPU_USB=pci_0000_01_00_2' /etc/libvirt/hooks/kvm.conf
+#sudo sed -i '$ a\VIRSH_GPU_SBC=pci_0000_01_00_3' /etc/libvirt/hooks/kvm.conf
+
+sudo sed -i '81s/.//' /etc/libvirt/libvirtd.conf
+sudo sed -i '104s/.//' /etc/libvirt/libvirtd.conf
+sudo sed -i '$ a\log_filters="1:qemu"' /etc/libvirt/libvirtd.conf
+sudo sed -i '$ a\log_outputs="1:file:/var/log/libvirt/libvirtd.log"' /etc/libvirt/libvirtd.conf
+
+sudo usermod -a -G libvirt $(whoami)
+
+sudo sed -i '/#user = "libvirt-qemu"/c \user = "ivo"' /etc/libvirt/qemu.conf
+sudo sed -i '/#group = "libvirt-qemu"/c \group = "wheel"' /etc/libvirt/qemu.conf
+
+sudo mkdir -p /var/lib/libvirt/vbios
+#sudo tar -C /var/lib/libvirt/vbios -xzvf gpu.tar.gz
+rm gpu.tar.gz
+#sudo chmod 644 /var/lib/libvirt/vbios/gpu.rom
+
+#-----------------------------------SAMBA-----------------------------------------
+
+sudo tar -C /etc/samba/ -xzvf smb.tar.gz
+rm smb.tar.gz
+
+#-----------------------------------SUDOERS---------------------------------------
+
+sudo sed -i '$ a\ivo    ALL=(ALL)    NOPASSWD: /usr/bin/virsh' /etc/sudoers
+sudo sed -i '$ a\ivo    ALL=(ALL)    NOPASSWD: /usr/bin/nvidia-persistenced' /etc/sudoers
+sudo sed -i '$ a\ivo    ALL=(ALL)    NOPASSWD: /usr/bin/nvidia-smi' /etc/sudoers
 
 # ---------------------------------SERVICES---------------------------------------
 
@@ -85,62 +160,65 @@ sudo systemctl enable teamviewerd
 echo "  teamviewerd enabled"
 sudo systemctl enable --now ckb-next-daemon
 echo "  iCue enabled and started"
-sudo systemctl enable --now zramd.service
-echo "  SWAP enabled and started"
+sudo systemctl enable --now libvirtd.service
+echo "  libvirtd enabled and started"
+sudo systemctl enable --now virtlogd.socket
+echo "  virtlogd enabled and started"
+sudo virsh net-autostart default
+echo "  net-autostart enabled"
+sudo virsh net-start default
+echo "  net-start enabled"
 
 # ---------------------------------FIREWALL---------------------------------------
-
-echo
-echo "ENABLING FIREWALL"
 
 sudo systemctl enable ufw.service --now
 echo "  firewall enabled and started"
 sudo ufw default deny
 sudo ufw enable
 sudo ufw allow from 192.168.0.0/24
-#sudo ufw allow 1714:1764/udp
-#sudo ufw allow 1714:1764/tcp
+sudo ufw allow 1714:1764/udp
+sudo ufw allow 1714:1764/tcp
 sudo ufw reload
 
 # ---------------------------------THEME---------------------------------------
 
-lookandfeeltool -a org.kde.breezedark.desktop
-/usr/lib/plasma-changeicons BeautyLine
-tar -xzvf config.tar.gz
-tar -xzvf icons.tar.gz
-tar -xzvf local.tar.gz
 tar -xzvf Dragon.tar.gz
-mv .config ~/
-mv .icons ~/
-mv .local ~/
-
 sudo mv Dragon /usr/share/sddm/themes/
-sudo rm -rf /usr/share/sddm/themes/maldives
-sudo rm -rf /usr/share/sddm/themes/maya
-sudo rm -rf /usr/share/sddm/themes/elarun
-sudo rm -rf /usr/share/sddm/themes/breeze
+
+tar -C /home/ivo/ -xzvf config.tar.gz
+rm config.tar.gz
+tar -C /home/ivo/ -xzvf icons.tar.gz
+rm icons.tar.gz
+tar -C /home/ivo/ -xzvf local.tar.gz
+
+rm local.tar.gz
+rm Dragon.tar.gz
+rm Omen.tar.gz
 
 # ---------------------------------FSTAB---------------------------------------
 
-#sudo sed -i '$ a # \t\t nvme1n1p3 - Linux SSD' /etc/fstab
-#sudo sed -i '$ a UUID=c388d55f-1412-4413-863d-a2e3104fc66a\t/media/btrfs/ssd\btrfs\tdefaults,rw,relatime\t0\t0' /etc/fstab
+sudo sed -i '$ a # \t\t nvme1n1p3 - Games SSD' /etc/fstab
+sudo sed -i '$ a #UUID=26d327c5-d5a7-4e39-a782-b9374951e0ee\t/media/btrfs/ssd\btrfs\tdefaults,rw,relatime\t0\t0' /etc/fstab
 
-#sudo sed -i '$ a #HDD' /etc/fstab
-#sudo sed -i '$ a # \t\t sda1 - NEXTCLOUD' /etc/fstab
-#sudo sed -i '$ a UUID=fdd0754d-49f0-4802-80ee-40f23d641fea\t/media/btrfs/nc\btrfs\tdefaults,rw,relatime\t0\t0' /etc/fstab
+sudo sed -i '$ a #HDD' /etc/fstab
+sudo sed -i '$ a # \t\t sdc1 - NEXTCLOUD' /etc/fstab
+sudo sed -i '$ a UUID=f4b349cb7-6aad-44a8-b1d5-105b4bcfd29d\t/media/btrfs/nc\btrfs\tdefaults,rw,relatime\t0\t0' /etc/fstab
 
-#sudo sed -i '$ a # \t\t sda2 - STORAGE' /etc/fstab
-#sudo sed -i '$ a UUID=2750d2ff-3da2-4904-9a9f-b73ae91a8fbe\t/media/btrfs/hdd\btrfs\tdefaults,rw,relatime\t0\t0' /etc/fstab
+sudo sed -i '$ a # \t\t sdc3 - HDD' /etc/fstab
+sudo sed -i '$ a UUID=130efac5-8fbe-4a27-80f8-97be0158e5a0\t/media/btrfs/hdd\btrfs\tdefaults,rw,relatime\t0\t0' /etc/fstab
 
-#sudo sed -i '$ a # \t\t sda3 - BACKUPS' /etc/fstab
-#sudo sed -i '$ a UUID=a3f1d11d-0fcc-46b5-9821-9801c2206b97\t/media/btrfs/bkp\btrfs\tdefaults,rw,relatime\t0\t0' /etc/fstab
+sudo sed -i '$ a # \t\t sdc2 - BACKUPS' /etc/fstab
+sudo sed -i '$ a UUID=3530600d-1f05-422a-8372-87ae1d0c5e27\t/media/btrfs/bkp\btrfs\tdefaults,rw,relatime\t0\t0' /etc/fstab
 
-#sudo sed -i '$ a #WINDOWS' /etc/fstab
-#sudo sed -i '$ a # \t\t nvme0n1p3 - Windows 10' /etc/fstab
-#sudo sed -i '$ a UUID=82062AED062AE1C1\t/media/btrfs/hdd\btrfs\tdefaults,rw,relatime\t0\t0' /etc/fstab
+sudo sed -i '$ a #WINDOWS' /etc/fstab
+sudo sed -i '$ a # \t\t sda3 - Windows 10' /etc/fstab
+sudo sed -i '$ a UUID=08E2109DE21090D4\t/media/ntfs/w-sys\ntfs-3g\tdefaults,rw,relatime\t0\t0' /etc/fstab
 
-#sudo sed -i '$ a # \t\t nvme0n1p4 - Windows SSD' /etc/fstab
-#sudo sed -i '$ a UUID=C88A20498A20367A\t/media/btrfs/bkp\btrfs\tdefaults,rw,relatime\t0\t0' /etc/fstab
+sudo sed -i '$ a # \t\t sda4 - Windows SSD' /etc/fstab
+sudo sed -i '$ a UUID=26F6AD3E032C2534\t/media/ntfs/w-ssd\ntfs-3g\tdefaults,rw,relatime\t0\t0' /etc/fstab
+
+sudo sed -i '$ a # \t\t sdb1 - Samsung' /etc/fstab
+sudo sed -i '$ a UUID=03B140B373965BED\t/media/ntfs/evo\ntfs-3g\tdefaults,rw,relatime\t0\t0' /etc/fstab
 
 # ---------------------------------REBOOT---------------------------------------
 
