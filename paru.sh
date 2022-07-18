658306b978cd5484039ceda07601f15823b2796f
Superhik <94892775+Sup3rhik@users.noreply.github.com>
2022-07-18 08:33:19 +0200

    Add files via upload

---
+++ paru.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+
+sudo pacman -Syu --noconfirm
+
+#-----------------------------------PARU----------------------------------
+
+git clone https://aur.archlinux.org/paru-bin
+cd paru-bin
+sudo pacman -Syu
+makepkg -si --noconfirm
+cd ..
+
+# ---------------------------------APPS-----------------------------------
+
+paru -S --noconfirm aic94xx-firmware wd719x-firmware brave-bin zramd
+sudo systemctl enable --now zramd.service
+echo "  SWAP enabled and started"
+
+echo
+/bin/echo -e "\e[1;32mD O N E\e[0m"
