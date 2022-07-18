836e514a0f065d0ed706b2533710522d64c54da9
Superhik <94892775+Sup3rhik@users.noreply.github.com>
2022-07-18 08:22:16 +0200

    Add files via upload

--- base.sh
+++ base.sh
@@ -25,8 +25,6 @@ echo root:PASSWD | chpasswd
 
 #----------------------------------APPS-----------------------------------
 
-reflector -c Croatia -a 6 --sort rate --save /etc/pacman.d/mirrorlist
-
 pacman -Sy --noconfirm --needed
 pacman -S --noconfirm base-devel linux-zen-headers linux-firmware grub efibootmgr dosfstools os-prober mtools networkmanager dialog wpa_supplicant wireless_tools nano wget reflector snapper btrfs-progs grub-btrfs duf dolphin konsole rsync ark unzip ntfs-3g kate bash-completion sof-firmware flatpak kinit ttf-droid ttf-hack ttf-font-awesome otf-font-awesome ttf-lato ttf-liberation ttf-linux-libertine ttf-opensans ttf-roboto ttf-ubuntu-font-family terminus-font ufw cronie ksysguard htop kfind sshfs samba openssh nfs-utils cups nmap print-manager cups-pdf grub-customizer
 
@@ -84,7 +82,6 @@ sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 #----------------------------------MULTILIB-------------------------------
 
-#Enable multilib
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
 #----------------------------------KDE------------------------------------
@@ -95,22 +92,7 @@ systemctl enable sddm
 echo " sddm enabled "
 sleep 1
 
-#----------------------------------PARU-----------------------------------
-
-git clone https://aur.archlinux.org/paru-bin
-cd paru-bin
-pacman -Syu
-makepkg -si
-cd ..
-
-paru -S --noconfirm snap-pac-grub snapper-gui
-
 #----------------------------------THEME----------------------------------
-
-tar -xzvf Archive.tar.gz
-tar -C /home/ivo/ -xzvf config.tar.gz
-tar -C /home/ivo/ -xzvf icons.tar.gz
-tar -C /home/ivo/ -xzvf local.tar.gz
 
 rm -rf /usr/share/sddm/themes/maldives
 rm -rf /usr/share/sddm/themes/maya
@@ -119,14 +101,14 @@ rm -rf /usr/share/sddm/themes/breeze
 
 #----------------------------------RAZNO-----------------------------------
 
-ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
-ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d/
-ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
-
 echo "Numlock=On" >> /etc/sddm.conf
 sed -i '12s/.//' /etc/profile.d/freetype2.sh
 echo "PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"" >> /etc/environment
 echo "EDITOR="/usr/bin/vim"" >> /etc/environment
+
+ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
+ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d/
+ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
 
 #----------------------------------EXIT----------------------------------
 printf "\e[1;32mDone! Type exit, umount -a and REBOOT.\e[0m"
